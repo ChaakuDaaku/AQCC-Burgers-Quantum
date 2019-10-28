Fixed gradient error

pde_vqe.py
@@ -54,13 +54,13 @@ def create_mini_batch(X, batch_size=256):
 
 
 def j_theta(var, f, g, h):
-    phi = tf.math.exp(-(h[0]-4*h[1])**2/(4*nu*(h[1]+1))) +\
-        tf.math.exp(-(h[0]-4*h[1]-2*np.pi)**2/(4*nu*(h[1]+1)))
+    phi = tf.math.exp(-(h[0]-4*h[1])**2/(4*nu*(h[1]+1)))
     qn = circuit(var, f)
     qn_shift = circuit(var, g)
     qn_init = circuit(var, h)
     with tf.GradientTape() as tape:
-        dphidx, dphidt = tape.gradient(phi, h)
+        phi += tf.math.exp(-(h[0]-4*h[1]-2*np.pi)**2/(4*nu*(h[1]+1)))
+    dphidx, dphidt = tape.gradient(phi, h)
     u0 = -(2 * nu / phi * dphidx) + 4.0
     with tf.GradientTape() as tape:
         with tf.GradientTape() as tt: